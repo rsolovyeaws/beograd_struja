bcc9006e52f7078e59e8d875b34ba483bff3f75e
Robert Solovev <rsolovye@gmail.com>
2024-11-23 19:06:18 +0100

    refactored street_state.py

--- telegram_app/bot/states/street_state.py
+++ telegram_app/bot/states/street_state.py
@@ -1,18 +1,25 @@
-from telegram.ext import ContextTypes
+"""Handles the street input state for the StreetState class."""
+
 from telegram import Update
+from telegram.ext import ContextTypes
+
 from telegram_app.bot.lang import PHRASES
-from telegram_app.bot.utils import STREET, HOUSE, send_message, validate_street
 from telegram_app.bot.states.state import State
+from telegram_app.bot.utils import HOUSE, STREET, send_message, validate_street
+
 
 class StreetState(State):
+    """Handles the street input state."""
+
     async def handle(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-        user_language = context.user_data['language']
+        """Handle the street input state."""
+        user_language = context.user_data["language"]
         phrases = PHRASES[user_language]
 
         if not validate_street(update.message.text):
-            await send_message(update, phrases['enter_street'])
+            await send_message(update, phrases["enter_street"])
             return STREET
 
-        context.user_data['street'] = update.message.text.upper()
-        await send_message(update, phrases['enter_house'])
+        context.user_data["street"] = update.message.text.upper()
+        await send_message(update, phrases["enter_house"])
         return HOUSE
