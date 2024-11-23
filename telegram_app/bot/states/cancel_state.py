03c5c483f703d4fcc83fbac275e14529237f5a2a
Robert Solovev <rsolovye@gmail.com>
2024-11-23 18:54:37 +0100

    refator cancel_state.py

--- telegram_app/bot/states/cancel_state.py
+++ telegram_app/bot/states/cancel_state.py
@@ -1,12 +1,19 @@
+"""Handles the cancellation of a conversation in the CancelState class."""
+
+from telegram import ReplyKeyboardRemove, Update
 from telegram.ext import ContextTypes, ConversationHandler
-from telegram import Update, ReplyKeyboardRemove
+
 from telegram_app.bot.lang import PHRASES
 from telegram_app.bot.states.state import State
 
+
 class CancelState(State):
+    """Handle the cancellation of a conversation."""
+
     async def handle(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-        user_language = context.user_data.get('language', 'English')
+        """Handle the cancellation of a conversation."""
+        user_language = context.user_data.get("language", "English")
         phrases = PHRASES[user_language]
 
-        await update.message.reply_text(phrases['bye'], reply_markup=ReplyKeyboardRemove())
+        await update.message.reply_text(phrases["bye"], reply_markup=ReplyKeyboardRemove())
         return ConversationHandler.END
