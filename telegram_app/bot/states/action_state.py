305a7461793b2a3a7c3bd7f21c91283125f3c1a6
Robert Solovev <rsolovye@gmail.com>
2024-11-23 18:49:53 +0100

    refactored ACTION state

--- telegram_app/bot/states/action_state.py
+++ telegram_app/bot/states/action_state.py
@@ -1,26 +1,50 @@
-from telegram.ext import ContextTypes
-from telegram import Update
-from telegram_app.bot.lang import PHRASES
-from telegram_app.bot.utils import AREA, LIST, DELETE, ACTION, get_address_keyboard, send_message, get_action_keyboard
-from telegram_app.bot.states.state import State
-from telegram_app.sql.models import User, Address
-from telegram_app.sql.queries import get_user, get_user_addresses
+"""Handles user actions such as adding, listing, and deleting addresses.
+
+This module contains the ActionState class.
+"""
+
 import logging
+
+from telegram import Update
+from telegram.ext import ContextTypes
+
+from telegram_app.bot.lang import PHRASES
+from telegram_app.bot.states.state import State
+from telegram_app.bot.utils import ACTION, AREA, DELETE, get_action_keyboard, get_address_keyboard, send_message
+from telegram_app.sql.queries import get_user, get_user_addresses
 
 logger = logging.getLogger(__name__)
 
+
 class ActionState(State):
-    async def handle(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Handles user actions such as adding, listing, and deleting addresses."""
+
+    async def handle(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:  # noqa: PLR0911
+        """Handle the callback query from the user and perform actions based on the query data.
+
+        Args:
+            update (Update): The update object that contains the callback query.
+            context (ContextTypes.DEFAULT_TYPE): The context object that contains user data.
+
+        Returns:
+            int: The next state of the conversation.
+
+        Actions:
+            - "Add": Prompts the user to enter an area and transitions to the AREA state.
+            - "List": Lists the user's subscribed addresses and transitions back to the ACTION state.
+            - "Delete": Prompts the user to select an address to delete and transitions to the DELETE state.
+
+        """
         query = update.callback_query
         await query.answer()
 
-        user_language = context.user_data['language']
+        user_language = context.user_data["language"]
         phrases = PHRASES[user_language]
 
-        if query.data == 'Add':
-            await query.edit_message_text(text=phrases['enter_area'])
+        if query.data == "Add":
+            await query.edit_message_text(text=phrases["enter_area"])
             return AREA
-        elif query.data == 'List':
+        if query.data == "List":
             user = get_user(update.effective_user.id)
 
             if not user:
@@ -30,29 +54,26 @@ class ActionState(State):
             addresses = get_user_addresses(user.id)
 
             if not addresses:
-                await send_message(update, phrases['no_addresses'])
-                
+                await send_message(update, phrases["no_addresses"])
+
                 # return to the action state
                 await query.message.reply_text(
                     phrases["choose_action"],
-                    reply_markup=get_action_keyboard(user_language)
+                    reply_markup=get_action_keyboard(user_language),
                 )
-            
+
                 return ACTION
 
             address_list = "\n".join([f"{addr.full_address}" for addr in addresses])
             response_message = f"{phrases['subscribed']}\n{address_list}"
             await send_message(update, response_message)
 
-            await query.message.reply_text(
-                phrases["choose_action"],
-                reply_markup=get_action_keyboard(user_language)
-            )
+            await query.message.reply_text(phrases["choose_action"], reply_markup=get_action_keyboard(user_language))
             return ACTION
 
-        elif query.data == 'Delete':
+        if query.data == "Delete":
             user = get_user(update.effective_user.id)
-            
+
             if not user:
                 await send_message(update, "User not found. Please subscribe first.")
                 return ACTION
@@ -60,28 +81,25 @@ class ActionState(State):
             addresses = get_user_addresses(user.id)
 
             if not addresses:
-                await send_message(update, phrases['no_addresses'])
-                
+                await send_message(update, phrases["no_addresses"])
+
                 # return to the action state
                 await query.message.reply_text(
                     phrases["choose_action"],
-                    reply_markup=get_action_keyboard(user_language)
+                    reply_markup=get_action_keyboard(user_language),
                 )
                 return ACTION
 
             # Save the addresses in the user's context (TODO: not sure why we need this)
-            context.user_data['addresses'] = addresses
-            
-            # Generate an interactive keyboard with the addresses
-            keyboard = get_address_keyboard(addresses, user_language)
-            
-            logger.info(f"User {user.telegram_id} is deleting an address.")
-            for i, addr in enumerate(addresses):
-                logger.info(f"Address {i+1}: {addr.id} - {addr.full_address}")
+            context.user_data["addresses"] = addresses
 
-            await query.message.reply_text(
-                phrases['subscribed'],
-                reply_markup=keyboard
-            )
-            
+            keyboard = get_address_keyboard(addresses, user_language)
+
+            logger.info("User %s is deleting an address.", user.telegram_id)
+            for i, addr in enumerate(addresses):
+                logger.info("Address %d: %s - %s", i + 1, addr.id, addr.full_address)
+
+            await query.message.reply_text(phrases["subscribed"], reply_markup=keyboard)
+
             return DELETE
+        return ACTION
