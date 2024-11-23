36a3e60d6f595d3f01cd8cfd7a1f864bacde5aa7
Robert Solovev <rsolovye@gmail.com>
2024-11-23 19:04:36 +0100

    refactored abc state.py

--- telegram_app/bot/states/state.py
+++ telegram_app/bot/states/state.py
@@ -1,9 +1,14 @@
+"""Defines the abstract base class for states in the Telegram bot."""
+
 from abc import ABC, abstractmethod
+
 from telegram import Update
 from telegram.ext import ContextTypes
 
 
 class State(ABC):
+    """Abstract base class for states in the Telegram bot."""
+
     @abstractmethod
-    async def handle(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        pass
+    async def handle(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        """Handle an update from the Telegram bot."""
